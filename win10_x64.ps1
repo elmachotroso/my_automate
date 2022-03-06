81a306ea09c2ed47ae00a9ac945e99fcb1c6c554
elmachotroso <me@andreivictor.net>
2022-03-06 09:27:03 +0800

    Initial powershell script for Win10 installation.

---
+++ win10_x64.ps1
@@ -0,0 +1,25 @@
+# Windows 10 powershell script to automate install of applications for new Win10 machines
+
+# install chocolatey
+Write-Ouput "Checking existence of Chocolatey..."
+
+# Install packages via chocolatey
+choco install 7zip -y
+choco install beyondcompare -y
+choco install android-studio -y
+choco install audacity -y
+choco install dotnet -y
+choco install filezilla -y
+choco install firefox -y
+choco install git -y
+choco install git-lfs -y
+choco install java -y
+choco install jr8 -y
+choco install k-lite -y
+choco install libreoffice -y
+choco install notepadplusplus -y
+choco install postman -y
+choco install putty -y
+choco install vcredist -y
+choco install veeam-agent -y
+choco install vscode -y
