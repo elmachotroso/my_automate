1ad368ac7edcc8c7e99ca3cba1f63fdb260ec69b
elmachotroso <me@andreivictor.net>
2022-04-29 22:53:42 +0800

    updated win10 powershell install script

--- win10_x64.ps1
+++ win10_x64.ps1
@@ -1,9 +1,10 @@
 # Windows 10 powershell script to automate install of applications for new Win10 machines
 
 # install chocolatey
-Write-Ouput "Checking existence of Chocolatey..."
+Write-Output "Checking existence of Chocolatey..."
 
 # Install packages via chocolatey
+choco install powertoys -y
 choco install 7zip -y
 choco install beyondcompare -y
 choco install android-studio -y
@@ -23,3 +24,5 @@ choco install putty -y
 choco install vcredist -y
 choco install veeam-agent -y
 choco install vscode -y
+choco install winlibs -y
+choco install cmake --installargs 'ADD_CMAKE_TO_PATH=System' -y
