cb7c7a5513a5063148490463b4613544e52ed0cd
elmachotroso <me@andreivictor.net>
2022-11-13 22:15:16 +0800

    Added vlc in win10_x64.ps1. Created family/lenovo_14alc05.ps1 for specific machines.

--- family/lenovo_14alc05.ps1
+++ family/lenovo_14alc05.ps1
@@ -13,13 +13,9 @@ Write-Host "Chocolatey found in $choco_location"
 # Install packages via chocolatey
 choco install powertoys -y
 choco install 7zip -y
-choco install beyondcompare -y
-choco install android-studio -y
 choco install audacity -y
 choco install dotnet -y
-choco install filezilla -y
 choco install firefox -y
-choco install barrier -y
 choco install git -y
 choco install git-lfs -y
 choco install java -y
@@ -27,13 +23,9 @@ choco install jr8 -y
 choco install k-lite -y
 choco install vlc -y
 choco install libreoffice -y
-choco install notepadplusplus -y
-choco install postman -y
 choco install putty -y
 choco install vcredist -y
 choco install veeam-agent -y
 choco install vscode -y
 choco install winlibs -y
-choco install epicgameslauncher -y
 choco install cmake --installargs 'ADD_CMAKE_TO_PATH=System' -y
-choco install unity-hub -y --ignore-checksums
